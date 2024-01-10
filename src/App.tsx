6b6ca36e1ed418fc7b95d71eb260a678ffbd0925
kimsunjae <woal9844@gmail.com>
2024-01-11 00:57:00 +0900

    chore : 초기 환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TestPage from "./pages/TestPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TestPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TestPage() {
+  return (
+    <div>
+      <h1>테스트 페이지임</h1>
+      <p>서버 첫 실행하면 오는 곳</p>
+    </div>
+  )
+}
+
+export default TestPage
