c82a456625c139301fd2c6061fd8fbc1c6bf0c59
heesu52 <heesu52@naver.com>
2024-01-14 02:45:53 +0900

    feat 로그인 페이지 style component로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,94 @@
 import React from 'react';
-import './Header.css';
+import styled, { createGlobalStyle } from 'styled-components';
 
-export default function Header() {
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+    font-family: 'Pretendard Variable', sans-serif;
+  }
+`;
+const HeaderContainer = styled.div`
+background-color: #FFF;
+width: 100vw;
+height: 70px;   //반응형으로 수정할 경우 함께 고쳐야 함, 모니터에 따라 달라짐
+display: flex;
+align-items: center;
+padding: 0 15%;
+border-bottom: 1px solid #E8E8E8;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const HeaderH2 = styled.h2`
+color: #650FA9;
+font-size: 27px;
+font-style: normal;
+font-weight: 600;
+line-height: 70px;
+margin-right: 10%;
+`;
+
+const HeaderInput = styled.input`
+text-indent: 20px;
+width: 370px;
+height: 40px;
+background-color: white;
+border: 1px solid #BDBDBD;
+border-radius: 5px;
+flex-shrink: 0;
+font-weight: 600;
+margin-right: 15%;
+`;
+
+const HeaderMember = styled.div`
+width: 100px;
+display: flex;
+align-items: center;
+
+`;
+
+const HeaderLogin = styled.a`
+color: #000;
+margin-right: 15%;
+flex-shrink: 0;
+font-size: 14px;
+&:hover,
+&:active {
+  color: #650FA9;
+}
+`;
+
+const HeaderSignup = styled.button`
+width: 70px;
+height: 40px;
+border-radius: 3px;
+border: 0.5px solid #BDBDBD;
+background: #FFF;
+box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.10);
+font-size: 14px;
+white-space: nowrap;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+export default function Header() {  
   return (
-    <div className="header_container">
-      <h2>StudyMate</h2>
-      <input type="text" placeholder= "검색 내용을 입력하세요" className="header_input" />
-      <div className="header_member">
-      <a href="#" className="header_login">로그인</a>
-      <input type="button" value= "회원가입" className="header_signup"/>
-      </div>
-    </div>
-  )
+    <>
+    <GlobalStyle />
+    <HeaderContainer>
+      <HeaderH2>StudyMate</HeaderH2>
+      <HeaderInput type="text" placeholder="검색 내용을 입력하세요" />
+      <HeaderMember>
+        <HeaderLogin href="#">로그인</HeaderLogin>
+        <HeaderSignup>회원가입</HeaderSignup>
+      </HeaderMember>
+    </HeaderContainer>
+    </>
+  );
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,24 +1,140 @@
 import React from 'react';
-import './Login.css';
+import styled, { createGlobalStyle } from 'styled-components';
+import google_login from "D:/Project/frontend/src/assets/images/google_login.png";
+import kakao_login from "D:/Project/frontend/src/assets/images/kakao_login.png";
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  border: none;
+}
+body {
+  font-family: "Pretendard Variable";
+  }
+
+`;
+const LoginWrapper = styled.div`
+margin-left: auto;
+margin-right: auto;
+width: 450px;
+height: 618px;
+padding: 40px;
+box-sizing: border-box;
+`;
+
+const LoginH2 = styled.h2`
+width: 100%;
+height: 40px;
+color: #650FA9;
+font-size: 35px;
+font-weight: 600;
+text-align: center;
+margin-bottom: 50px;
+margin-left: auto;
+margin-right: auto;
+`;
+
+const LoginInput = styled.input`
+text-indent: 20px;
+width: 100%;
+height: 48px;
+padding: 0 10px;
+box-sizing: border-box;
+background-color: #F8F8F8;
+border-radius:5px;
+margin-bottom: 16px;
+font-size: 14px;
+`;
+
+const LoginSubmit = styled.input`
+width: 100%;
+height: 55px;
+border-radius: 10px;
+border: 1px solid var(--Gray-03, #BDBDBD);
+background: var(--bdbdbd, #650FA9);
+color: var(--White, #FFF);
+text-align: center;
+font-size: 16px;
+font-weight: 600;
+margin-top: 20px;
+`;
+
+const FindMore = styled.p`
+width: 100%;
+height: 17px;
+margin-top: 10px;
+text-align: center;
+`;
+
+const FindIt = styled.a`
+font-size: 12px;
+font-weight: 600;
+line-height: normal;
+padding-left: 14px;
+padding-right: 14px;
+color: #bdbdbd;
+&:hover,
+&:active {
+  color: #650FA9;
+}
+`;
+
+const LoginH4 = styled.h4`
+height: 20px;
+text-align: center;
+font-size: 16px;
+font-weight: 600;
+line-height: normal; 
+color: #BDBDBD;
+margin-top: 40px;
+`;
+
+const SubmitBtn = styled.div`
+text-align: center;
+    height: 75px;
+`;
+
+const KakaoSubmit = styled.input`
+background-image: url(${kakao_login});
+background-size: cover;
+border-radius: 5px;
+width: 175px;
+height: 45px;
+margin-top: 15px;
+display: inline-block;
+`;
+const GoogleSubmit = styled.input`
+background-image: url(${google_login});
+background-size: cover;
+width: 175px;
+height: 40px;
+position: absolute;
+margin-top:35px;
+left: 50%;
+transform: translate(-50%, 100%);
+`;
+
 
 export default function Login() {
   return (
-    <div className="login_wrapper">
-      <h2>Login to StudyMate</h2>
-        <input type="email" placeholder= "이메일" className="input" />
-        <input type="password" placeholder= "비밀번호" className="input" />
-        <input type="submit" value= "로그인" className='login_submit'/>
-        <p className="find_more">
-          <a href="#" className="find_it">아이디(이메일)찾기</a>
-          <a href="#" className="find_it">비밀번호 찾기</a>
-          <a href="#" className="find_it">회원가입</a>
-        </p>
-        <h4>간편로그인</h4>
-        <div className='submit_btn'>
-        <input type="submit" value= "" className='kakao_submit'/>
-        <input type="submit" value= "" className='google_submit'/>
-        </div>
-        
-    </div>
-  )
+    <>
+      <GlobalStyle />
+      <LoginWrapper>
+        <LoginH2>Login to StudyMate</LoginH2>
+        <LoginInput type="email" placeholder="이메일" />
+        <LoginInput type="password" placeholder="비밀번호" />
+        <LoginSubmit type="submit" value="로그인" />
+        <FindMore>
+          <FindIt href="#">아이디(이메일)찾기</FindIt>
+          <FindIt href="#">비밀번호 찾기</FindIt>
+          <FindIt href="#">회원가입</FindIt>
+        </FindMore>
+        <LoginH4>간편로그인</LoginH4>
+        <SubmitBtn>
+          <KakaoSubmit type="submit" value="" />
+          <GoogleSubmit type="submit" value="" />
+        </SubmitBtn>
+      </LoginWrapper>
+    </>
+  );
 }
