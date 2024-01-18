81af5d5d6c9df0fe3939d884dc1503ac9b6dd1c3
heesu52 <heesu52@naver.com>
2024-01-19 03:24:00 +0900

    feat: 하단바>Navbar로 변경하여 구현, 이미지 아직 미첨부

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+
+import offlineimage from '../assets/images/offline.png'
+import mypageimage from '../assets/images/mypage.png'
+import studyingimage from '../assets/images/studying.png'
+import postimage from '../assets/images/post.png'
+import onlineimage from '../assets/images/online.png'
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+
+    background-color: #fff;
+  }
+`
+const FrameContainer = styled.nav`
+  background-color: #fff;
+  width: 100vw;
+  height: 40px;
+  border-bottom: 1px solid #e8e8e8;  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: sticky;
+  top: 10vh;
+`
+
+const TextGroup = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+
+`
+const NavbarClick = styled.a`
+color: #000;
+font-size: 14px;
+font-weight: 500;
+white-space: nowrap;
+margin: 15%;
+padding: 5px;
+
+&:hover,
+  &:active {
+    font-weight: bold;
+    color: #650fa9;
+     background-color: rgba(220, 196, 239, 0.3); /* #dcc4ef의 60% 투명한 버전 */
+     border-radius: 3px;
+  }
+`
+
+
+
+
+export default function Navbar() {
+    return (
+     <>
+      <GlobalStyle/>
+      <FrameContainer>
+          <TextGroup>
+            <NavbarClick href="/api/login">오프라인 매칭</NavbarClick>
+            <NavbarClick href="/api/login">온라인 매칭</NavbarClick>
+            <NavbarClick href="/api/login">게시판</NavbarClick>
+            <NavbarClick href="/api/login">스터디 기록</NavbarClick>
+            <NavbarClick href="/api/login">마이페이지</NavbarClick>
+        </TextGroup>
+      </FrameContainer>
+    </>
+  );
+}
+
