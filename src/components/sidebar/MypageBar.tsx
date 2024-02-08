db489b6856458ca87aed0b03c74271406e2ee9c4
kimsunjae <woal9844@gmail.com>
2024-02-08 14:05:54 +0900

    feat : sidebar 조건부 스타일링 적용

--- src/components/sidebar/MypageBar.tsx
+++ src/components/sidebar/MypageBar.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Container = styled.div`
   display: flex;
@@ -15,53 +15,90 @@ const Container = styled.div`
 
 const Profile = styled(Link)`
   width: 240px;
-  height: 55px;
   font-size: 25px;
   display: flex;
   margin-top: 50px;
   align-items: center;
-  border: 2px solid;
   text-decoration: none;
   color: inherit;
+  padding: 20px 0px 20px 40px;
+  border-radius: 20px;
 `
 const Notification = styled(Link)`
   width: 240px;
-  height: 55px;
   font-size: 25px;
   display: flex;
   align-items: center;
   text-decoration: none;
   color: inherit;
+  padding: 20px 0px 20px 40px;
+  border-radius: 20px;
 `
 
 const MyPost = styled(Link)`
   width: 240px;
-  height: 55px;
   font-size: 25px;
   display: flex;
   align-items: center;
   text-decoration: none;
   color: inherit;
+  padding: 20px 0px 20px 40px;
+  border-radius: 20px;
 `
 
 const MatchingList = styled(Link)`
   width: 240px;
-  height: 55px;
   font-size: 25px;
   display: flex;
   align-items: center;
   margin-bottom: 50px;
   text-decoration: none;
   color: inherit;
+  padding: 20px 0px 20px 40px;
+  border-radius: 20px;
 `
 
 export default function MypageBar() {
+  const location = useLocation()
+
   return (
     <Container>
-      <Profile to="/mypage">프로필</Profile>
-      <Notification to="/mypage/notification">알림</Notification>
-      <MyPost to="/mypage/mypost">내 게시물</MyPost>
-      <MatchingList to="/mypage/matchinglist">매칭 기록</MatchingList>
+      <Profile
+        to="/mypage"
+        style={{
+          backgroundColor: location.pathname === '/mypage' ? '#E8DCF2' : 'inherit',
+          color: location.pathname === '/mypage' ? '#650FA9' : 'inherit',
+          fontWeight: location.pathname === '/mypage' ? 'bold' : 'inherit',
+        }}>
+        프로필
+      </Profile>
+      <Notification
+        to="/mypage/notification"
+        style={{
+          backgroundColor: location.pathname === '/mypage/notification' ? '#E8DCF2' : 'inherit',
+          color: location.pathname === '/mypage/notification' ? '#650FA9' : 'inherit',
+          fontWeight: location.pathname === '/mypage/notification' ? 'bold' : 'inherit',
+        }}>
+        알림
+      </Notification>
+      <MyPost
+        to="/mypage/mypost"
+        style={{
+          backgroundColor: location.pathname === '/mypage/mypost' ? '#E8DCF2' : 'inherit',
+          color: location.pathname === '/mypage/mypost' ? '#650FA9' : 'inherit',
+          fontWeight: location.pathname === '/mypage/mypost' ? 'bold' : 'inherit',
+        }}>
+        내 게시물
+      </MyPost>
+      <MatchingList
+        to="/mypage/matchinglist"
+        style={{
+          backgroundColor: location.pathname === '/mypage/matchinglist' ? '#E8DCF2' : 'inherit',
+          color: location.pathname === '/mypage/matchinglist' ? '#650FA9' : 'inherit',
+          fontWeight: location.pathname === '/mypage/matchinglist' ? 'bold' : 'inherit',
+        }}>
+        매칭 기록
+      </MatchingList>
     </Container>
   )
 }
