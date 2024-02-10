ca6b341cc570d13afe196db9dd44dc7e17f02940
kimsunjae <woal9844@gmail.com>
2024-02-11 00:58:45 +0900

    design : mypage -> 내 게시물 퍼블리싱

--- src/pages/Mypage/MatingListPage.tsx
+++ src/pages/Mypage/MatingListPage.tsx
@@ -1,15 +1,31 @@
 import Header2 from '../../components/Header2.tsx'
 import MypageBar from '../../components/sidebar/Mypagebar.tsx'
 import Navbar2 from '../../components/Navbar2.tsx'
+import styled from 'styled-components'
 
-function MatingListPage() {
+const Container = styled.div`
+  display: flex;
+  margin-top: 100px;
+`
+
+const MatchingListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: calc(100% - 400px);
+`
+
+function MatchingListPage() {
   return (
     <div>
       <Header2 />
       <Navbar2 />
-      <MypageBar />
+      <Container>
+        <MypageBar />
+        <MatchingListWrapper></MatchingListWrapper>
+      </Container>
     </div>
   )
 }
 
-export default MatingListPage
+export default MatchingListPage
