2f2ea0d379ef43e02f148beb4acab251481fdece
kimsunjae <woal9844@gmail.com>
2024-02-12 16:44:38 +0900

    fix : 로그인 페이지 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,21 @@
 import Login from '../components/Login.tsx'
-import Header from '../components/Header.tsx'
+import Header2 from '../components/Header2.tsx'
+import { styled } from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 109px);
+`
 
 function LoginPage() {
   return (
     <div>
-      <Header />
-      <Login />
+      <Header2 />
+      <Container>
+        <Login />
+      </Container>
     </div>
   )
 }
