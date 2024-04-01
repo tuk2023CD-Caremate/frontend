4b8572de8cd0122ec812044e18ed89448678c2cd
kimsunjae <woal9844@gmail.com>
2024-04-01 17:59:00 +0900

    design : 메인헤더 디자인 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,94 +1,55 @@
+import styled from 'styled-components'
+import LogoImg from '../assets/images/StudyMate.svg'
 import { Link } from 'react-router-dom'
-import styled, { createGlobalStyle } from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
-* {
-  font-family: "Pretendard";
-  margin: 0;
-  padding: 0;
-}
-body {
-    background-color: #fff;
-  }
-`
-const HeaderContainer = styled.div`
-  background-color: #fff;
-  width: 100%;
-  height: 60px;
-  border-bottom: 1px solid #e8e8e8;
+const Container = styled.div`
   display: flex;
-  align-items: center;
   justify-content: space-between;
-`
-
-const HeaderLogo = styled.a`
-  color: #650fa9;
-  font-size: 27px;
-  font-weight: bold;
-  margin-left: 15%;
-  &:hover,
-  &:active {
-    color: #650fa9;
-  }
-`
-
-const HeaderInput = styled.input`
-  text-indent: 20px;
-  width: 370px;
-  height: 40px;
-  background-color: white;
-  border: 1px solid #bdbdbd;
-  border-radius: 5px;
-  flex-shrink: 0;
-  font-weight: 600;
-`
-
-const HeaderMember = styled.div`
-  display: flex;
   align-items: center;
-  margin-right: 15%;
+  height: 109px;
+  padding-left: 140px;
+  padding-right: 140px;
 `
 
-const HeaderLogin = styled.a`
-  color: #000;
-  flex-shrink: 0;
-  font-size: 14px;
-  margin-right: 10%;
-  &:hover,
-  &:active {
-    color: #650fa9;
-    font-weight: bold;
-  }
+const Logo = styled.img`
+  width: 236px;
 `
 
-const HeaderSignup = styled.button`
-  width: 70px;
-  height: 40px;
-  border-radius: 3px;
-  border: 0.5px solid #bdbdbd;
-  background: #fff;
-  box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.1);
-  font-size: 14px;
-  white-space: nowrap;
+const RightWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Btn = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  border-radius: 10px;
+  background-color: #ffffff;
+  border: solid 1px #bdbdbd;
+  width: 120px;
+  height: 50px;
+  font-size: 20px;
+  font-weight: bold;
+  margin: 30px;
+  cursor: pointer;
 `
 
 export default function Header() {
   return (
-    <>
-      <GlobalStyle />
-      <HeaderContainer>
-        <HeaderLogo href="/">StudyMate</HeaderLogo>
-        <HeaderInput type="text" placeholder="검색 내용을 입력하세요" />
-        <HeaderMember>
-          <HeaderLogin href="/login">로그인</HeaderLogin>
-          <Link to="/signin">
-            <HeaderSignup> 회원가입</HeaderSignup>
-          </Link>
-        </HeaderMember>
-      </HeaderContainer>
-    </>
+    <Container>
+      <Link to={'/'}>
+        <Logo src={LogoImg} />
+      </Link>
+      <RightWrapper>
+        <Link to={'/login'}>
+          <Btn>로그인</Btn>
+        </Link>
+        <Link to={'/signup'}>
+          <Btn>회원가입</Btn>
+        </Link>
+      </RightWrapper>
+    </Container>
   )
 }
