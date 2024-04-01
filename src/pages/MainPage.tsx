9ff57c187f8618d87293c79130b68c32470cf250
kimsunjae <woal9844@gmail.com>
2024-04-01 18:03:46 +0900

    design : 백그라운드 이미지 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,19 @@
 import Header from '../components/Header'
+import styled from 'styled-components'
+import onboarding1 from '../assets/images/onboarding1.png'
+
+const Container = styled.div`
+  background-image: url(${onboarding1});
+  width: 100dvw; /* 적절한 너비 설정 */
+  height: 100dvh; /* 적절한 높이 설정 */
+  background-size: cover; /* 이미지가 컨테이너에 맞게 확대되도록 설정 */
+`
 
 function MainPage() {
   return (
     <div>
       <Header />
+      <Container></Container>
     </div>
   )
 }
