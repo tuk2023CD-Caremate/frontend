226587c44b06d7896c6c8ae0e6c142e1419e8ba8
kimsunjae <woal9844@gmail.com>
2024-04-04 00:48:12 +0900

    useOnScreen ts error 수정

--- src/components/main/useOnScreen.ts
+++ src/components/main/useOnScreen.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 
 // 요소가 화면에 보일 때 콜백 함수를 실행할 수 있도록 해주는 커스텀 훅
-function useOnScreen(ref: { current: Element }) {
+function useOnScreen(ref: { current: Element | null }) {
   const [isIntersecting, setIntersecting] = useState(false)
 
   useEffect(() => {
+    if (!ref.current) return
+
     const observer = new IntersectionObserver(
       ([entry]) => {
         setIntersecting(entry.isIntersecting)
@@ -12,14 +14,14 @@ function useOnScreen(ref: { current: Element }) {
       { threshold: 0.3 }, // 화면의 30%가 보일경우
     )
 
-    if (ref.current) {
-      observer.observe(ref.current)
-    }
+    observer.observe(ref.current)
 
     return () => {
-      observer.unobserve(ref.current)
+      if (ref.current) {
+        observer.unobserve(ref.current)
+      }
     }
-  }, [])
+  }, [ref])
 
   return isIntersecting
 }
