6dcae5788762bb06629d0f4f8dd4c0a5b2fa8289
Heesu Jang <heesu52@naver.com>
2024-04-04 23:36:44 +0900

    refactor: moment를 dayjs로 수정

--- src/components/StudyCalendar.tsx
+++ src/components/StudyCalendar.tsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
-import moment from 'moment'
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
 import { useState } from 'react'
+
+dayjs.locale('ko'); 
 
 const StyledCalendarWrapper = styled.div`
   width: 100%;
@@ -69,33 +72,30 @@ const StyledCalendar = styled(Calendar)`
   }
 `
 
-type ValuePiece = Date | null
-type Value = ValuePiece | [ValuePiece, ValuePiece]
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-const StudyCalendar = () => {
-  const today = new Date()
-  const [date, setDate] = useState<Value>(today)
-
+const  StudyCalendar = () => {
+  const today = new Date();
+  const [date, setDate] = useState<Value>(today);
+  
   const handleDateChange = (newDate: Value) => {
-    setDate(newDate)
-  }
-
+    setDate(newDate);
+  };
+  
   return (
-    <StyledCalendarWrapper>
-      <StyledCalendar
-        value={date}
-        onChange={handleDateChange}
-        formatDay={(date) => moment(date).format('D')} // 일 제거 숫자만 보이게
-        formatYear={(date) => moment(date).format('YYYY')} // 네비게이션 눌렀을때 숫자 년도만 보이게
-        formatMonthYear={(date) => moment(date).format('YYYY. MM')} // 네비게이션에서 2023. 12 이렇게 보이도록 설정
-        calendarType="gregory" // 일요일 부터 시작
-        showNeighboringMonth={false} // 전달, 다음달 날짜 숨기기
-        next2Label={null} // +1년 & +10년 이동 버튼 숨기기
-        prev2Label={null} // -1년 & -10년 이동 버튼 숨기기
-        minDetail="year" // 10년단위 년도 숨기기
-      />
-    </StyledCalendarWrapper>
-  )
+  	<StyledCalendarWrapper>
+        <StyledCalendar
+          value={date}
+          onChange={handleDateChange}
+          formatDay={(date) => dayjs(date).format("D")} // 일 제거 숫자만 보이게
+          formatMonthYear={(date) => dayjs(date).format("YYYY. MM")} // 네비게이션에서 2023. 12 이렇게 보이도록 설정
+          calendarType="gregory" // 일요일 부터 시작
+          next2Label={null} // +1년 & +10년 이동 버튼 숨기기
+          prev2Label={null} // -1년 & -10년 이동 버튼 숨기기
+        />
+      </StyledCalendarWrapper>
+  );
 }
 
 export default StudyCalendar
