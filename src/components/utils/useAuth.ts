56b95c787cde69b6a4f85807ed5ddaeec98fb1e2
kimsunjae <woal9844@gmail.com>
2024-05-01 22:11:16 +0900

    fix : ProtecetedRoute 작동하지 않던 오류 수정

--- src/components/utils/useAuth.ts
+++ src/components/utils/useAuth.ts
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react'
 
 export default function useAuth() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('accessToken')
     setIsAuthenticated(!!token)
+    setLoading(false)
   }, [])
 
-  return isAuthenticated
+  return { isAuthenticated, loading }
 }
