9e2b1289f4b8935c6a2255f104df96632204c146
Heesu Jang <heesu52@naver.com>
2024-05-07 16:29:05 +0900

    design: 스터디 기록 수정된 피그마로 퍼블리싱

---
+++ src/components/PieChart.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
+interface CalenderItem {
+  id: number;
+  studyClass: string;
+  entiretime: string;
+  starttime: string;
+  endtime : string;
+}
+interface PieChartProps {
+  calenderList: CalenderItem[];
+}
+const timeToSeconds = (time:string) : number => {
+  const [hours, minutes, seconds] = time.split(':').map(Number);
+  return hours * 3600 + minutes * 60 + seconds;
+};
+const PieChart = ({ calenderList }: PieChartProps) => {
+   const labels = calenderList.map(item => item.studyClass);
+   const totalSeconds = calenderList.reduce((total, item) => total + timeToSeconds(item.entiretime), 0);
+   const percentages= calenderList.map(item => (timeToSeconds(item.entiretime) / totalSeconds) * 100);
+ 
+   const [chartData] = useState({
+    labels: labels,
+    datasets: [{
+      label: '# of Votes',
+      data: percentages,
+      backgroundColor: [
+        '#7AC0F2', // 하늘색
+        '#D75983', // 분홍색
+        '#FFDA79', // 추가된 색상
+        '#B983FF'  // 추가된 색상
+      ],
+    }],
+  })
+  const options = {
+    plugins: {
+      datalabels: {
+        formatter: (_value: any, context: any) => {
+          return context.chart.data.labels[context.dataIndex];
+        },
+      },
+      legend: {
+        display: true,
+        responsive : true,
+        position: 'right' as const,
+        labels : {
+          color: "#000000",
+          padding : 15,
+          font: { 
+            weight: 'bold' as const,
+            size : 28,
+          },
+        }
+      },
+    },
+  };
+  return (
+    <Pie data={chartData} options={options} />
+  );
+};
+export default PieChart;
